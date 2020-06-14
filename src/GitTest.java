67ff3e52634eb996c86873b41e4207fb736e0825
lidi <932335110@qq.com>
2020-06-14 20:21:35 +0800

    老李程序员

---
+++ src/GitTest.java
@@ -0,0 +1,10 @@
+/**
+ * @author LiDi
+ * @className GitTest
+ * @date 2020/06/14/20:18
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("你好!Git,期待与你合作!!!!");
+    }
+}
